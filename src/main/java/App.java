d8be39193822328f91062696a3cde51fccdccb07
Andrew Dang <andrewmtdang@gmail.com>
2025-10-29 09:46:06 -0400

    small testing changes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,6 +5,10 @@ import java.sql.Statement;
 import java.util.Properties;
 
 public class App {
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/school";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
     public static void main(String[] args) throws Exception {
 
         System.out.println("hello");
--- target/classes/Database.sql
+++ target/classes/Database.sql
@@ -1,3 +1,7 @@
+CREATE DATABASE school;
+
+\c school
+
 CREATE TABLE IF NOT EXISTS students (
 	student_id INT GENERATED ALWAYS AS IDENTITY,
 	first_name VARCHAR(50) NOT NULL,
